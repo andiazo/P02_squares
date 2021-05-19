15d07adc23862038b1c7a128473be56e578cc249
Andrés Díaz <36974713+andiazo@users.noreply.github.com>
2021-05-19 18:55:41 -0500

    Add files via upload

--- AlphaGOAgentProgram.java
+++ AlphaGOAgentProgram.java
@@ -7,6 +7,7 @@ package uniltiranyu.examples.games.squares;
 
 import static java.lang.Integer.max;
 import static java.lang.Integer.min;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -28,8 +29,47 @@ public class AlphaGOAgentProgram implements AgentProgram {
      * Creates a Dummy agent for playing squares
      * @param color Player's color
      */
+    
     public AlphaGOAgentProgram( String color ){
         this.color = color;        
+    }
+    
+    public TreeNode createGameTree(Percept p) {
+        
+        boolean maxPlayer = p.get(Squares.TURN).equals(color);
+        
+        // Build de board
+        int size = Integer.parseInt((String)p.get(Squares.SIZE));
+        int[][][] board = new int[size][size][4];
+        
+        for ( int i = 0; i < size; i++ ) {
+            for ( int j = 0; j < size; j++ ) {
+                
+                if(((String)p.get(i+":"+j+":"+Squares.LEFT)).equals(Squares.TRUE))
+                    board[i][j][0] = 1;
+                else
+                    board[i][j][0] = 0;
+                
+                if(((String)p.get(i+":"+j+":"+Squares.TOP)).equals(Squares.TRUE))
+                    board[i][j][1] = 1;
+                else
+                    board[i][j][1] = 0;
+                
+                if(((String)p.get(i+":"+j+":"+Squares.BOTTOM)).equals(Squares.TRUE))
+                    board[i][j][2] = 1;
+                else
+                    board[i][j][2] = 0;
+                
+                if(((String)p.get(i+":"+j+":"+Squares.RIGHT)).equals(Squares.TRUE))
+                    board[i][j][3] = 1;
+                else
+                    board[i][j][3] = 0;
+                
+            }
+        }
+
+        TreeNode node = new TreeNode(board);
+        return node;
     }
     
     // Who get more points function
@@ -48,7 +88,7 @@ public class AlphaGOAgentProgram implements AgentProgram {
     // Alpha Beta Algorithm  
     public int decisionAlphaBeta(TreeNode node, int depth, int alpha, int beta, boolean maxPlayer) {
         int value;
-        if (depth == 0) {
+        if (depth == 0 || node.children == null) {
             return evaluate(node);
         }
         if (maxPlayer) {
@@ -76,6 +116,69 @@ public class AlphaGOAgentProgram implements AgentProgram {
         }
     }
     
+    public ArrayList<String> linesOnBox(Percept p, int row, int col){
+        ArrayList<String> possibleMovements = new ArrayList<String>();
+        if(((String)p.get(row+":"+col+":"+Squares.LEFT)).equals(Squares.FALSE)) {
+            possibleMovements.add(Squares.LEFT);
+            System.out.println("|||||| left ||||||||");
+        }
+        if(((String)p.get(row+":"+col+":"+Squares.TOP)).equals(Squares.FALSE)) {
+            possibleMovements.add(Squares.TOP);
+            System.out.println("|||||| top ||||||||");
+        }
+        if(((String)p.get(row+":"+col+":"+Squares.BOTTOM)).equals(Squares.FALSE)) {
+            possibleMovements.add(Squares.BOTTOM);
+            System.out.println("|||||| bottom ||||||||");
+        }
+        if(((String)p.get(row+":"+col+":"+Squares.RIGHT)).equals(Squares.FALSE)) {
+            possibleMovements.add(Squares.RIGHT);
+            System.out.println("|||||| right ||||||||");
+        }
+        return possibleMovements;
+    }
+    
+    //
+    public int[] getAdjacent(Percept p, int row, int col, ArrayList<String> possibleMovements) {
+        
+        int[] adjacents = new int[4];
+        int size = Integer.parseInt((String)p.get(Squares.SIZE));
+        int linesLeft = 5, linesBottom = 5, linesRight = 5, linesTop = 5;
+        
+        for (String pm: possibleMovements) {
+            switch(pm) {
+                case "left":
+                    if (col - 1 >= 0) {
+                        linesLeft = 4-linesOnBox(p, row, col-1).size();
+                    }
+                    break;
+                case "top":
+                    if (row - 1 >= 0) {
+                        linesTop = 4-linesOnBox(p, row-1, col).size();
+                    }   
+                    break;
+                case "right":
+                    if (col + 1 < size) {
+                        linesRight = 4-linesOnBox(p, row, col+1).size();
+                    }
+                    break;
+                case "bottom":
+                    if (row + 1 < size) {
+                        linesBottom = 4-linesOnBox(p, row+1, col).size();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        adjacents[0] = linesLeft;
+        adjacents[1] = linesBottom;
+        adjacents[2] = linesRight;
+        adjacents[3] = linesTop;
+        
+        return adjacents;
+    }
+    
     @Override
     /**
      * Computes an action to be carried by the agent according to the perception
@@ -84,31 +187,86 @@ public class AlphaGOAgentProgram implements AgentProgram {
      */
     public Action compute(Percept p) {
         // Determines if it is the agents turn
+        
         if( p.get(Squares.TURN).equals(color) ){
             // Gets the size of the board
             int size = Integer.parseInt((String)p.get(Squares.SIZE));
-            int i = 0;
-            int j = 0;
             // Gets an square randomly and try to draw a border if possible
-            Array<String> v = new Array<String>();
-            while(v.size()==0){
-              i = (int)(size*Math.random());
-              j = (int)(size*Math.random());
-              // p.get(i+":"+j+":"+Squares.LEFT)
-              // gets "true" if the left border of square (i,j) is set, "false" otherwise
-              if(((String)p.get(i+":"+j+":"+Squares.LEFT)).equals(Squares.FALSE))
-                v.add(Squares.LEFT);
-              if(((String)p.get(i+":"+j+":"+Squares.TOP)).equals(Squares.FALSE))
-                v.add(Squares.TOP);
-              if(((String)p.get(i+":"+j+":"+Squares.BOTTOM)).equals(Squares.FALSE))
-                v.add(Squares.BOTTOM);
-              if(((String)p.get(i+":"+j+":"+Squares.RIGHT)).equals(Squares.FALSE))
-                v.add(Squares.RIGHT);
+            String v = new String();
+            ArrayList<String> pm = new ArrayList<String>();
+            int[] adjs = new int[4];
+            int count = 0;
+            int pos = -1;
+            int[] ij = new int[2];
+                    
+            inicio:
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    pm = linesOnBox(p, i, j);
+                    count = 4-pm.size();
+                    System.out.println("Count: "+count+"| pm:"+pm+"| i:"+i+"| j:"+j);
+                    if (count < 2) {
+                        adjs = getAdjacent(p, i, j, pm);
+                        for(int k = 0; k < 4; k++) {
+                            if (adjs[k] < 2) {
+                                pos = k;
+                                break;
+                            }
+                        }
+                        
+                       
+                        switch(pos) {
+                            case 0:
+                                if(((String)p.get(i+":"+j+":"+Squares.LEFT)).equals(Squares.FALSE))
+                                    v = Squares.LEFT;
+                                else 
+                                    v = Squares.TOP;
+                                break;
+                            case 1:
+                                if(((String)p.get(i+":"+j+":"+Squares.BOTTOM)).equals(Squares.FALSE))
+                                    v = Squares.BOTTOM;
+                                else 
+                                    v = Squares.TOP;
+                                break;
+                            case 2:
+                                if(((String)p.get(i+":"+j+":"+Squares.LEFT)).equals(Squares.FALSE))
+                                    v = Squares.RIGHT;
+                                else {
+                                    v = Squares.TOP;
+                                }
+                                break;
+                            case 3:
+                                v = Squares.TOP;
+                                break;
+
+                            default:
+                                v = Squares.TOP;
+                                System.out.println("NO POSIBLE MOVEMENTS");
+                                break;
+                        }
+                        System.out.println(pm);
+
+                        ij[0] = i;
+                        ij[1] = j;
+                        pos = -1;
+                        break inicio;
+                    }
+                    else {
+                        ij[0] = 0;
+                        ij[1] = 1;
+                        v = Squares.TOP;
+                        System.out.println("Si es ESTE");
+                        pos = -1;
+                    }
+                }
             }
             try{
-            	String move = v.get((int)(Math.random()*v.size()));
+            	String move = v;
+                int i = ij[0];
+                int j = ij[1];
             	// Action( i+":"+j+":"+Squares.BOTTOM ) 
             	// draws bottom border of square (i,j)
+                System.out.println("AG: "+color+" "+i+":"+j+":"+move);
                 return new Action( i+":"+j+":"+move);
             }catch(Exception e){}
         }
@@ -127,10 +285,15 @@ class TreeNode {
     int[] move;
     TreeNode parent;
     List<TreeNode> children;
-
+    
     public TreeNode(int[][][] board, int[] move) {
         this.board = board;
         this.move = move;
+        this.children = new LinkedList<TreeNode>();
+    }
+    
+    public TreeNode(int[][][] board) {
+        this.board = board;
         this.children = new LinkedList<TreeNode>();
     }
 
